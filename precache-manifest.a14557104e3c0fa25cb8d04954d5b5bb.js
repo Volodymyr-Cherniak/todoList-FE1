ad1260892c14ebd60bf77afbc3ae30125fe761d2
Volodymyr-Cherniak <vovastalker26@gmail.com>
2020-03-05 14:28:10 +0200

    Updates

---
+++ precache-manifest.a14557104e3c0fa25cb8d04954d5b5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3a39670ba5849ff463a41e8707bae8",
+    "url": "/todoList-FE1/index.html"
+  },
+  {
+    "revision": "1c517b22a5f1d67a3644",
+    "url": "/todoList-FE1/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "71db971c842f13061b13",
+    "url": "/todoList-FE1/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "1c517b22a5f1d67a3644",
+    "url": "/todoList-FE1/static/js/2.16ea285f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todoList-FE1/static/js/2.16ea285f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71db971c842f13061b13",
+    "url": "/todoList-FE1/static/js/main.ef2def11.chunk.js"
+  },
+  {
+    "revision": "b6468a2b17dd6a1ee667",
+    "url": "/todoList-FE1/static/js/runtime-main.1a684775.js"
+  }
+]);
